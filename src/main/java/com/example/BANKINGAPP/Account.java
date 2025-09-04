13d845f9c582ca201c4b38a3bef30019a88c2060
karthikng <karthikng777@gmail.com>
2025-09-04 11:25:09 +0530

    Initial commit

---
+++ src/main/java/com/example/BANKINGAPP/Account.java
@@ -0,0 +1,32 @@
+package com.example.BANKINGAPP;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_holder_name", nullable = false)
+    private String accountHolderName;
+
+    @Column(nullable = false)
+    private double balance = 0.0;
+
+    // No-args constructor (required by JPA)
+    public Account() {
+    }
+
+    // All-args constructor (optional but useful)
+    public Account(String accountHolderName, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+}
